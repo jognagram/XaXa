a62e8951388c8cd73bf6f61c6817d672464e6ea3
Jogna <jordan.gnamien@smile.ci>
2024-06-26 14:04:13 +0000

    jogna

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -8,21 +8,37 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Center(
-        child: Column(
-          children: [
-            Center(
-              child: Text("Bienvenu"),
-            ),
-            TextButton(
+        appBar: AppBar(
+            title: Text('Commander un Taxi'),
+            leading: IconButton(
               onPressed: () => _signOut(context),
-              child: Text("Deconnexion"),
-            )
-          ],
-        ),
-      ),
-    ));
+              icon: const Icon(Icons.logout_outlined),
+            )),
+        body: SingleChildScrollView(
+          child: Column(children: [
+            Center(
+                child: Image.asset(
+              'assets/logo/map.png',
+              height: 500,
+            )),
+          ]),
+        ));
+    //  return Scaffold(
+    //      body: SafeArea(
+    //    child: Center(
+    //      child: Column(
+    //        children: [
+    //          Center(
+    //            child: Text("Bienvenu"),
+    //          ),
+    //          TextButton(
+    //            onPressed: () => _signOut(context),
+    //            child: Text("Deconnexion"),
+    //          )
+    //        ],
+    //      ),
+    //    ),
+    //  ));
   }
 
   void _signOut(BuildContext context) async {
